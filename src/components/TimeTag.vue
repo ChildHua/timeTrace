41903d72081047285b0ab047eb6dd4429743a9b6
hdrun <hdrun911@gmail.com>
2018-06-15 20:07:28 +0800

    add time tag component

---
+++ src/components/TimeTag.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="col-xs-4 col-sm-4">
+            <button class="btn btn-default btn-lg btn-block"
+                    :style="{'background-color':tag.color}"
+                    v-for="(tag,key) in this.$store.state.timeTags">
+                {{tag.name}}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "time-tag"
+    }
+</script>
+
+<style scoped>
+
+</style>
